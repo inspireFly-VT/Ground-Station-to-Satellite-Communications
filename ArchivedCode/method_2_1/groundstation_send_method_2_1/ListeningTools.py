26d4133e98f68bd9da14a13f746140939b5c6a60
WillsMcGraw <willsmcgraw21@vt.edu>
2024-12-02 19:24:17 -0500

    Update ListeningTools.py

--- ArchivedCode/method_2_1/groundstation_send_method_2_1/ListeningTools.py
+++ ArchivedCode/method_2_1/groundstation_send_method_2_1/ListeningTools.py
@@ -46,12 +46,17 @@ def send_corrupted_packets(packetList: list) -> bytes:
     for i in range(len(packetList)):
         if packetList[i] is None:
             corruptedList.append(i)
+    print(corruptedList)
             
     # This is the callsign for our satellite and ground station
     callsign = "K4KDJ"
 
     # Converts the list of corrupted/missing packets into a byte string, where each entry is 2 bytes long
-    corruptedListBytes = b''.join(struct.pack('h', num) for num in corruptedList)
+    corruptedListBytes = b''
+    
+    for index in corruptedList:
+        indexBytes = index.to_bytes(2, "big")
+        corruptedListBytes += indexBytes
     
     # Converts the list of corrupted indicies byte string into an AX25 frame to be sent
     corruptedListFrame = DataToAX25_2_1.encode_ax25_frame(corruptedListBytes, callsign, callsign, b'\x00', 1)
@@ -60,141 +65,3 @@ def send_corrupted_packets(packetList: list) -> bytes:
     # Sleep for a second to give the satellite time to start receiving
     time.sleep(1.0)
     return corruptedListFrame
-    
-    
-
-
-# def continuousListening(packetList: list, packetNumber: int) -> list:
-#     """
-#     Takes a list to add packet data to. Then, it listens for send messages
-#     for certain amount of time. For each packet the ground station receives,
-#     the packet is decoded and the packet data is stored.
-# 
-#     Args:
-#         packetList (list): The packet list for packet data to be added to.
-# 
-#     Returns:
-#         list: The packet list with the updated packet data
-#     """
-#     # Creates a counter to keep count of the number of times the while loop below is run
-#     # Creates a boolean check to see if the last packet is received or not
-#     continueSending = True
-#     listeningCounter = 0
-# 
-#     while listeningCounter < (packetNumber) and continueSending:
-#         # Increments listening counter
-#         listeningCounter += 1
-#         # Optionally change the receive timeout from its default of 0.5 seconds:
-#         packet = rfm.receive(timeout=1.0)
-#         # If no packet was received during the timeout then None is returned.
-#         if packet is None:
-#             # Packet has not been received
-#             print("Received nothing! Listening again...")
-#         else:
-#             # Received a packet!
-#             # Print out the raw bytes of the packet:        
-#             print(f"Received (raw bytes): {packet}")
-#             # And decode to ASCII text and print it too.  Note that you always
-#             # receive raw bytes and need to convert to a text format like ASCII
-#             # if you intend to do string processing on your data.  Make sure the
-#             # sending side is sending ASCII data before you try to decode!
-# 
-#             
-#             # Decodes packet
-#             operatingMode, fcsCorrect, data, packetIndex = DataToAX25_2_1.decode_ax25_frame(packet)
-#             packetIndex = int.from_bytes(packetIndex, "big")
-#             
-#             # Asks for packet again if fcsCorrect is false
-#             # If fcsCorrect is false, no confirmation message is sent to the satellite
-#             # Then satellite should send it again
-#             if not fcsCorrect:
-#                 print("fcs not correct")
-#     #             rfm.send(None)
-#                 continue
-#             
-#             packetList[packetIndex] = data
-#             try:
-#                 packet_text = str(packet, "ascii")
-#                 print(f"Received (ASCII) at index {packetIndex}: {packet_text}")
-#             except UnicodeError:
-#                 print("Hex data: ", [hex(x) for x in packet])
-# 
-
-#             
-#             if packetIndex == numberOfPackets - 1:
-#                 continueSending = False
-#     return packetList
-# 
-# # This is the callsign for our satellite and ground station
-# callsign = "K4KDJ"
-#     
-# # Declares a variable to store the number of packets being sent
-# numberOfPackets = -1
-#     
-# # Tells the satellite to send the number of packets
-# rfm.send("Send number of packets")
-# 
-# while True:
-#     # Optionally change the receive timeout from its default of 0.5 seconds:
-#     packet = rfm.receive(timeout=5.0)
-#     
-#     # If no packet was received during the timeout then None is returned.
-#     if packet is None:
-#         # Packet has not been received
-#         print("Received nothing! Listening again...")
-#         
-#     else:
-#         # Received a packet!
-#         # Print out the raw bytes of the packet:        
-#         print(f"Received (raw bytes): {packet}")
-#         # And decode to ASCII text and print it too.  Note that you always
-#         # receive raw bytes and need to convert to a text format like ASCII
-#         # if you intend to do string processing on your data.  Make sure the
-#         # sending side is sending ASCII data before you try to decode!
-#         
-#         # Decodes the packet, the data field should hold a byte representation of the number
-#         # of packets that will be sent
-#         operatingMode, fcsCorrect, size, dataIndex = DataToAX25_2_1.decode_ax25_frame(packet)
-#         
-#         # Checks fcsCorrect value to check for corruption. If fcsCorrect is false,
-#         # no confirmation message is sent so the satellite resends info.
-#         if not fcsCorrect:
-#             print("fcs not correct")
-#             continue
-#         
-#         # Converts 
-#         numberOfPackets = int.from_bytes(size, 'big')
-#         break;
-#         
-# # Prints out the number of expected packets
-# print(f"Expected number of packets is {numberOfPackets}.")
-# # Creates an empty list of the expected size, filled with None values
-# # Packet data will be stored in this
-# packetList = []
-# for i in range(numberOfPackets):
-#     packetList.append(None)
-#     
-# # Tells satellite to send packets
-# rfm.send("Send packets")
-# 
-# # Wait to receive packets.
-# print("Waiting for packets...")
-# packetList = continuousListening(packetList, numberOfPackets)
-# 
-# corruptedPackets = checkCorruptedPackets(packetList)
-# while len(corruptedPackets) != 0:
-#     # Converts the list of corrupted/missing packets into a byte string, where each entry is 2 bytes long
-#     corruptedIndicesBytes = b''.join(struct.pack('h', num) for num in corruptedPackets)
-#     
-#     # Converts the corrupted indicies byte string into an AX25 frame to be sent
-#     corruptedIndicesFrame = DataToAX25_2_1.encode_ax25_frame(corruptedIndicesBytes, callsign, callsign, b'\x00', 1)
-#     
-#     # Sends AX25 frame of corrupted indicies
-#     rfm.send(corruptedIndicesFrame)
-#     
-#     # Listens for satellite response with new packets
-#     packetList = continuousListening(packetList, len(corruptedPackets))
-#     
-#     # Rechecks packetList to update the corruptedPacketList
-#     corruptedPackets = checkCorruptedPackets(packetList)
-#     print(corruptedPackets)
